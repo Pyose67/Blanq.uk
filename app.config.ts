84267e4f1c5562f6a7fd7a67f03d9ec12631d274
edzin <edmarma92@gmail.com>
2026-05-03 02:35:20 -0300

    EU VOU FICAR MALUCO5

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,28 @@
-import { defineConfig } from 'vinxi'
+import { createApp } from 'vinxi'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-export default defineConfig({
+export default createApp({
+  routers: [
+    {
+      name: 'public',
+      type: 'static',
+      dir: './public',
+    },
+    {
+      name: 'client',
+      type: 'client',
+      handler: './app/entry-client.tsx',
+      target: 'browser',
+      base: '/_build',
+    },
+    {
+      name: 'ssr',
+      type: 'http',
+      handler: './app/entry-server.tsx',
+      target: 'server',
+    },
+  ],
   vite: {
     plugins: [
       tsconfigPaths(),
