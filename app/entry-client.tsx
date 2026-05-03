f9c26e24262699f4db94caffc48c97a2b2dff982
edzin <edmarma92@gmail.com>
2026-05-03 14:33:03 -0300

    fix: use original lovable config, output to dist

--- app/entry-client.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from '@tanstack/react-router'
-import { createRouter } from './router'
-
-const router = createRouter()
-
-createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
-)
