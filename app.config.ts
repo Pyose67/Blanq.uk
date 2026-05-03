638010947c60dd54e22683bf526e8abaae5c2572
edzin <edmarma92@gmail.com>
2026-05-03 02:05:11 -0300

    RESOLVENDO ERRO ENCAPETADO DO DOMÍNI10

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vinxi'
+import { createApp } from 'vinxi'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
+export default createApp({
+  routers: [
+    {
+      name: 'public',
+      type: 'static',
+      dir: './public',
+    },
+    {
+      name: 'ssr',
+      type: 'http',
+      handler: './app/entry-server.tsx',
+      target: 'server',
+    },
+    {
+      name: 'client',
+      type: 'client',
+      handler: './app/entry-client.tsx',
+      target: 'browser',
+      base: '/_build',
+    },
+  ],
   vite: {
     plugins: [
       tsconfigPaths(),
