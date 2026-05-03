33ea3fa0551ba3148395c486f1955215e5a141d8
edzin <edmarma92@gmail.com>
2026-05-03 11:44:54 -0300

    whole entry server alterado

--- app/entry-server.tsx
+++ app/entry-server.tsx
@@ -1,12 +1,28 @@
 import { defineEventHandler } from 'h3'
 
-export default defineEventHandler(() => {
+export default defineEventHandler(async () => {
+  const clientManifest = import.meta.env.MANIFEST['client']
+  const assets = await clientManifest.inputs[clientManifest.handler].assets()
+
+  const assetTags = assets
+    .map((asset: any) => {
+      if (asset.tag === 'script') {
+        return `<script type="module" src="${asset.attrs.src}"></script>`
+      }
+      if (asset.tag === 'link') {
+        return `<link rel="stylesheet" href="${asset.attrs.href}" />`
+      }
+      return ''
+    })
+    .join('\n')
+
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Blanq</title>
+  ${assetTags}
 </head>
 <body>
   <div id="root"></div>
