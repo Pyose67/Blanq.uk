58cefe0851d6c36911b778552d597531134a913c
edzin <edmarma92@gmail.com>
2026-05-03 03:17:34 -0300

    ultima tentativa de hoje

--- app.config.ts
+++ app.config.ts
@@ -1,35 +1,12 @@
-import { createApp } from 'vinxi'
+import { defineConfig } from '@tanstack/react-start/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-export default createApp({
-  routers: [
-    {
-      name: 'public',
-      type: 'static',
-      dir: './public',
-    },
-    {
-      name: 'client',
-      type: 'client',
-      handler: './app/entry-client.tsx',
-      target: 'browser',
-      base: '/_build',
-    },
-    {
-      name: 'ssr',
-      type: 'http',
-      handler: './app/entry-server.tsx',
-      target: 'server',
-    },
-  ],
+export default defineConfig({
+  tsr: {
+    routesDirectory: './app/routes',
+    generatedRouteTree: './app/routeTree.gen.ts',
+  },
   vite: {
-    plugins: [
-      tsconfigPaths(),
-      TanStackRouterVite({
-        routesDirectory: './app/routes',
-        generatedRouteTree: './app/routeTree.gen.ts',
-      }),
-    ],
+    plugins: [tsconfigPaths()],
   },
 })
