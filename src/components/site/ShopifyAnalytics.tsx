09a5a56b8f640b92b4de99bd6af1da274f397a66
edzin <edmarma92@gmail.com>
2026-05-17 22:17:23 -0300

    chore: trigger redeploy

--- src/components/site/ShopifyAnalytics.tsx
+++ src/components/site/ShopifyAnalytics.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react"; // v2
 import { useRouterState } from "@tanstack/react-router";
 import {
   sendShopifyAnalytics,
